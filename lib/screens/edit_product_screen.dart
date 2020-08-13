e4e0f444af4a9a3ff9eeef526032d9c00af6ada1
dadzi <dadziemikke2@gmail.com>
2020-08-13 13:03:26 +0000

    1.4.3

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -44,6 +44,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
     _form.currentState.save();
   }
 
@@ -70,6 +74,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide a value';
+                  }
+                  return null;
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
