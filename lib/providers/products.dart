5eed333b797df5be1c17399c89f9f3969add5bb9
Michael Dadzie <dadziemikke2@gmail.com>
2020-08-17 15:09:28 +0000

    1.5.4

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -118,9 +118,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  void updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = 'https://soulllshop.firebaseio.com/products/$id.json';
+      //try catch
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {}
