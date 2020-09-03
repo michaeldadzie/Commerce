55a223248340c5334ca1416b173512651e649312
Michael Dadzie <dadziemikke2@gmail.com>
2020-09-03 10:59:37 +0000

    1.5.4

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/auth.dart';
@@ -19,19 +18,6 @@ class AuthScreen extends StatelessWidget {
       // resizeToAvoidBottomInset: false,
       body: Stack(
         children: <Widget>[
-//          Container(
-//            decoration: BoxDecoration(
-//              gradient: LinearGradient(
-//                colors: [
-//                  Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
-//                  Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
-//                ],
-//                begin: Alignment.topLeft,
-//                end: Alignment.bottomRight,
-//                stops: [0, 1],
-//              ),
-//            ),
-//          ),
           SingleChildScrollView(
             child: Container(
               height: deviceSize.height,
@@ -40,35 +26,6 @@ class AuthScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-//                  Flexible(
-//                    child: Container(
-//                      margin: EdgeInsets.only(bottom: 20.0),
-//                      padding:
-//                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
-//                      transform: Matrix4.rotationZ(-8 * pi / 180)
-//                        ..translate(-10.0),
-//                      // ..translate(-10.0),
-//                      decoration: BoxDecoration(
-//                        borderRadius: BorderRadius.circular(20),
-//                        color: Colors.deepOrange.shade900,
-//                        boxShadow: [
-//                          BoxShadow(
-//                            blurRadius: 8,
-//                            color: Colors.black26,
-//                            offset: Offset(0, 2),
-//                          )
-//                        ],
-//                      ),
-//                      child: Text(
-//                        'MyShop',
-//                        style: TextStyle(
-//                          color: Theme.of(context).primaryColor,
-//                          fontSize: 50,
-//                          fontWeight: FontWeight.normal,
-//                        ),
-//                      ),
-//                    ),
-//                  ),
                   Flexible(
                     flex: deviceSize.width > 600 ? 2 : 1,
                     child: AuthCard(),
