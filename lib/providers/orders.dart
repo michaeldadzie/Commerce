cbbc9f5bd895453cf1fe47890fb9afad3cd7a6f2
Michael Dadzie <dadziemikke2@gmail.com>
2020-09-03 10:58:26 +0000

    1.5.4

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -19,12 +19,14 @@ class OrderItem {
 
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
+  final String authToken;
+  Orders(this.authToken, this._orders);
   List<OrderItem> get orders {
     return [..._orders];
   }
 
   Future<void> fetchAndSetOrders() async {
-    const url = 'https://soulllshop.firebaseio.com/orders.json';
+    final url = 'https://soulllshop.firebaseio.com/orders.json?auth=$authToken';
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -55,7 +57,7 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    const url = 'https://soulllshop.firebaseio.com/orders.json';
+    final url = 'https://soulllshop.firebaseio.com/orders.json?auth=$authToken';
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
