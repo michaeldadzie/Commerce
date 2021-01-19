4347c6c259502a8a2257c6a9bb4b65509c7e7c2c
Michael Dadzie <dadziemikke2@gmail.com>
2021-01-19 18:16:13 +0000

    Improve UI Code

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: IconButton(
-          icon: Icon(
-            EvilIcons.spinner,
-            size: 80,
-            color: Colors.red,
-          ),
-        ),
+        child: CircularProgressIndicator()
       ),
     );
   }
