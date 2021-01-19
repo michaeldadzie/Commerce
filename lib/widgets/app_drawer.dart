adbe06239a31e247f322343bc706bd13af77faf1
Michael Dadzie <dadziemikke2@gmail.com>
2021-01-19 18:16:26 +0000

    Improve UI Code

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/auth.dart';
 import 'package:shop_app/screens/orders_screen.dart';
@@ -9,24 +10,47 @@ import 'package:shop_app/helpers/custom_route.dart';
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String greetingMessage() {
+      var timeNow = DateTime.now().hour;
+
+      if (timeNow <= 12) {
+        return 'Good Morning ';
+      } else if ((timeNow > 12) && (timeNow <= 16)) {
+        return 'Good Afternoon ';
+      } else if ((timeNow > 16) && (timeNow < 23)) {
+        return 'Good Evening ';
+      } else {
+        return 'Good Night ';
+      }
+    }
+
     return Drawer(
       child: Column(
         children: [
           AppBar(
-            title: Text('Good Morning'),
+            title: Text(
+              greetingMessage(),
+              style: GoogleFonts.raleway(),
+            ),
             automaticallyImplyLeading: false,
           ),
           Divider(),
           ListTile(
             leading: Icon(Icons.store),
-            title: Text('Shop'),
+            title: Text(
+              'Shop',
+              style: GoogleFonts.raleway(fontWeight: FontWeight.bold),
+            ),
             onTap: () {
               Navigator.of(context).pushReplacementNamed('/');
             },
           ),
           ListTile(
             leading: Icon(EvilIcons.credit_card),
-            title: Text('Orders'),
+            title: Text(
+              'Orders',
+              style: GoogleFonts.raleway(fontWeight: FontWeight.bold),
+            ),
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(OrdersScreen.routeName);
@@ -40,7 +64,10 @@ class AppDrawer extends StatelessWidget {
           Divider(),
           ListTile(
             leading: Icon(Icons.edit),
-            title: Text('Manage Products'),
+            title: Text(
+              'Manage Products',
+              style: GoogleFonts.raleway(fontWeight: FontWeight.bold),
+            ),
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(UserProductsScreen.routeName);
@@ -48,7 +75,10 @@ class AppDrawer extends StatelessWidget {
           ),
           ListTile(
             leading: Icon(Icons.exit_to_app),
-            title: Text('Logout'),
+            title: Text(
+              'Logout',
+              style: GoogleFonts.raleway(fontWeight: FontWeight.bold),
+            ),
             onTap: () {
               Navigator.of(context).pop();
               Navigator.of(context).pushReplacementNamed('/');
