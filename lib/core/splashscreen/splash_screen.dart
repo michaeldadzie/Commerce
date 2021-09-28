0ac439e568911b324f5399586ac8fdcfc6f78be9
dadzi <dadziemikke2@gmail.com>
2021-09-28 21:35:21 +0100

    Added temporal splash screen when authenticating user

--- lib/core/splashscreen/splash_screen.dart
+++ lib/core/splashscreen/splash_screen.dart
@@ -4,9 +4,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator()
-      ),
+      body: CircularProgressIndicator(),
     );
   }
 }
