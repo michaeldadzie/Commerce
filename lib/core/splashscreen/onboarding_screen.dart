e40fdb796540075308853c48fd5521ee2d6ec96b
dadzi <dadziemikke2@gmail.com>
2021-09-30 18:09:02 +0100

    Refactored code

--- lib/core/splashscreen/onboarding_screen.dart
+++ lib/core/splashscreen/onboarding_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/core/splashscreen/intro.dart';
-import 'package:shop_app/core/splashscreen/intro_content.dart';
+import 'package:shop_app/core/splashscreen/intro_content.dart' as ic;
+
+// enum AuthMode { Signup, Login }
 
 class OnboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
+    ic.SizeConfig().init(context);
     return Scaffold(
       body: Intro(),
     );
