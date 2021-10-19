8f5595d4cd0595dfa15b44821f228dc198b4fadc
dadzi <dadziemikke2@gmail.com>
2021-10-19 21:17:46 +0100

    Initialized firebase when the app starts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/core/splashscreen/intro.dart';
@@ -17,7 +18,9 @@ import 'features/home/presentation/providers/products.dart';
 import 'core/utils/theme.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: Colors.transparent, // status bar color
@@ -67,7 +70,7 @@ class MyApp extends StatelessWidget {
                       authResultSnapshot.connectionState ==
                               ConnectionState.waiting
                           ? SplashScreen()
-                          : OnboardScreen(),
+                          : AuthScreen(),
                 ),
           routes: {
             AuthScreen.routeName: (ctx) => AuthScreen(),
