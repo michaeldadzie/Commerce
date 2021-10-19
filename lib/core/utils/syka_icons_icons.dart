e1629d85be52e6b54b06a110eb3cd74d06194da9
dadzi <dadziemikke2@gmail.com>
2021-10-19 21:07:40 +0100

    Added icon widget

---
+++ lib/core/utils/syka_icons_icons.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+
+class SykaIcons {
+  SykaIcons._();
+
+  static const _kFontFam = 'SykaIcons';
+  // static const String _kFontPkg = '';
+  static const String _kFontPkg = null;
+
+  static const IconData dashboard =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData more =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData received =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData send =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData sent =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData transaction =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData account =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
