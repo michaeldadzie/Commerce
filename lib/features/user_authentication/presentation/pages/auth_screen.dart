2cc65cd77e3fdcb8f7ef2e42b981e2da77be3ca4
dadzi <dadziemikke2@gmail.com>
2021-10-21 00:33:09 +0100

    Improved UI code

--- lib/features/user_authentication/presentation/pages/auth_screen.dart
+++ lib/features/user_authentication/presentation/pages/auth_screen.dart
@@ -23,54 +23,57 @@ class AuthScreen extends StatelessWidget {
     final deviceSize = MediaQuery.of(context).size;
     // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
     // transformConfig.translate(-10.0);
-    return Scaffold(
-      // resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: NeverScrollableScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 40.h),
-              Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  'Welcome, Sign Up or Log In',
-                  style: GoogleFonts.raleway(
-                      fontSize: 25.h, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        // resizeToAvoidBottomInset: false,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: NeverScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 40.h),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    'Welcome, Sign Up or Log In',
+                    style: GoogleFonts.raleway(
+                        fontSize: 25.h, fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              SizedBox(height: 10.h),
-              Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  'to start your Soulll Shop experience',
-                  style: GoogleFonts.raleway(
-                      fontSize: 16.h, color: Colors.grey[500]),
+                SizedBox(height: 10.h),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    'to start your Soulll Shop experience',
+                    style: GoogleFonts.raleway(
+                        fontSize: 16.h, color: Colors.grey[500]),
+                  ),
                 ),
-              ),
-              Stack(
-                children: [
-                  SingleChildScrollView(
-                    physics: NeverScrollableScrollPhysics(),
-                    child: Container(
-                      height: 500,
-                      width: deviceSize.width,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Flexible(
-                            flex: deviceSize.width > 600 ? 2 : 1,
-                            child: AuthCard(),
-                          ),
-                        ],
+                Stack(
+                  children: [
+                    SingleChildScrollView(
+                      physics: NeverScrollableScrollPhysics(),
+                      child: Container(
+                        height: 500,
+                        width: deviceSize.width,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Flexible(
+                              flex: deviceSize.width > 600 ? 2 : 1,
+                              child: AuthCard(),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
